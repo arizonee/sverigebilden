5e824f27900f2d6f271149b8e72bb85df86af1a3
Axel Engström <axel@axelca.com>
2018-01-02 19:31:07 +0100

    skicka vidare användaren när filmen är slut

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
-// Hämta den sidan användaren begär (ex. ?sida=omoss)
-$sida = $_GET["sida"];
-
 // Inkludera headern
 include("includes/header.php");
 
 // Om användaren har begärt en specifik undersida hämta den
-if ($sida != "") {
-  include("content/" . $sida . ".php");
-  // Om användaren inte har begärt en specifik undersida hämta video.php
+if (isset($_GET["sida"])) {
+  include("content/" . $_GET["sida"] . ".php");
+// Om användaren inte har begärt en specifik undersida hämta video.php
 } else {
   include("content/video.php");
 }
+
 // Inkludera footern
 include("includes/footer.php");
 ?>
--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-console.log($("div.ruta"))
-var rutor=$("div.ruta")
+/*
+// Lagra rutorna på startsite i minnet
+var rutor = $("div.ruta")
+
+
+// För varje ruta, räkna om dess höjd
 rutor.each(function (ruta) {
   $(this).css("height", $(this).width())
+});
+*/
 
+// Lagra videon i minnet
+var video = $("video")
+
+// När videon är slut, skicka vidare användaren
+video.on('ended', function() {
+  window.location.href = window.location.href + "?sida=startite"
 })
