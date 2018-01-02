3161ef4ad428c159b9da114a7c997de29a0fc7b6
Axel Engström <axel@axelca.com>
2018-01-02 19:32:14 +0100

    ändrade för att slippa en php-varning

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 include("includes/header.php");
 
 // Om användaren har begärt en specifik undersida hämta den
+// Skrev om det här lite för att slippa en varning
 if (isset($_GET["sida"])) {
   include("content/" . $_GET["sida"] . ".php");
 // Om användaren inte har begärt en specifik undersida hämta video.php
