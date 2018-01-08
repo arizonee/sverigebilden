b909576623969fcf9b0e6f8841d350e336af2fd8
Ricardo Burrows <ricardoburrows@mrmacbook-sama.lan>
2018-01-08 14:51:01 +0100

    Skapat p-klassen artikeltext i style.css, gjort en narkotikagrafik

--- content/narkotika.php
+++ content/narkotika.php
@@ -5,17 +5,26 @@
     </div>
   </div>
   <div class="row">
-    <div class="col-sm-5">
-      <canvas id="narkotika-statistik1" width="250" height="250"></canvas>
+    <div class="col-lg-12 col-5">
+      <canvas id="narkotika-statistik1" width="250" height="115"></canvas>
     </div>
     <div class="col-sm-2">
     </div>
     <div class="col-sm-5">
-      <canvas id="narkotika-statistik2" width="250" height="250"></canvas>
     </div>
   </div>
   <div class="row row-bottom">
     <div class="col-lg-12 col-12">
+        <p class="artikeltext">
+            I Sverige finns det idag tre olika statistikserier över narkotikarelaterade dödsfall, och alla mäter narkotikadödligheten på olika sätt. <br>
+            <b>EMCDDA</b> är det internationella narkotikaindexet som enbart mäter underliggande dödsorsak. Underliggande dödsorsak är enligt WHO den sjukdom eller skadeorsak som startade den kedja av sjukdomshändelser som direkt ledde till döden. <br>
+            <b>Socialstyrelsens</b> register mäter narkotikarelaterade dödsfall i både underliggande dödsorsak och bidragande dödsorsak. Den siffran är därför högre än det internationella indexet.<br>
+            <b>Toxreg</b> bygger på rättsmedicinskt undersökta dödsfall. De redovisar likt Socialstyrelsen narkotikarelaterade dödsfall i både underliggande dödsorsak och bidragande dödsorsak. Den siffran är dock lägre vilket beror på att det inte är alla döda som obduceras.
+            <br>
+            <br>
+            Sverigebilden har valt att lyfta fram statistik från EMCDDA. Enligt både Socialstyrelsen och EU:s narkotikabyrå kan Sveriges ökade dödstal till viss del bero på utvecklade metoder för att spåra narkotika vid oklara dödsfall, samt ändringar i hur statistik samlas in och rapporteras. Trots det räknas vi ha ett av de högsta dödstalen i EU. Hos samtliga våra nordiska grannländer är dödsfallen omkring en tredjedel av Sveriges.
+
+        </p>
     </div>
   </div>
 </div>
--- js/script.js
+++ js/script.js
@@ -96,21 +96,17 @@ if (document.getElementById("kott-statistik2")) {
 if (document.getElementById("narkotika-statistik1")) {
   var ctx = document.getElementById("narkotika-statistik1").getContext('2d');
   var myChart = new Chart(ctx, {
-      type: 'pie',
+      type: 'line',
       data: {
-          labels: ["1970", "1990", "2016"],
+          labels: ["1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015"],
           datasets: [{
-              label: 'miljoner kg kött/hela landet',
-              data: [407.7, 458.7, 815.8],
+              label: 'Antal döda av narkotika överdos',
+              data: [70, 122, 133, 138, 157, 194, 204, 203, 258, 225, 245, 235, 310, 320, 350, 369, 371, 427, 476, 614, 661],
               backgroundColor: [
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(240, 170 ,20 , 0.2)',
-                  'rgba(255, 99, 132, 0.2)'
+                  'rgba(0, 153, 0, 0.2)',
               ],
               borderColor: [
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(240, 170, 20, 1)',
-                  'rgba(255,99,132,1)'
+                  'rgba(0, 153, 0, 1)',
               ],
               borderWidth: 1
           }]
@@ -135,48 +131,6 @@ if (document.getElementById("narkotika-statistik1")) {
   });
 }
 
-// Tabell narkotika2
-if (document.getElementById("narkotika-statistik2")) {
-  var ctx = document.getElementById("narkotika-statistik2").getContext('2d');
-  var myChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-          labels: ["1970", "1990", "2016"],
-          datasets: [{
-              label: 'kg kött/person',
-              data: [50.6, 53.6, 82.2],
-              backgroundColor: [
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(240, 170 ,20 , 0.2)',
-                  'rgba(255, 99, 132, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(240, 170, 20, 1)',
-                  'rgba(255,99,132,1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  gridLines: {
-                      color: 'rgba(255, 255, 255, 0.2)'
-                  },
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }],
-              xAxes: [{
-                  gridLines: {
-                      color: 'rgba(255, 255, 255, 0.2)'
-                  }
-              }]
-          }
-      }
-  });
-}
 
 // Tabell utsläpp1
 if (document.getElementById("utslapp-statistik1")) {
@@ -188,49 +142,6 @@ if (document.getElementById("utslapp-statistik1")) {
           datasets: [{
               label: 'miljoner kg kött/hela landet',
               data: [407.7, 458.7, 815.8],
-              backgroundColor: [
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(240, 170 ,20 , 0.2)',
-                  'rgba(255, 99, 132, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(240, 170, 20, 1)',
-                  'rgba(255,99,132,1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  gridLines: {
-                      color: 'rgba(255, 255, 255, 0.2)'
-                  },
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }],
-              xAxes: [{
-                  gridLines: {
-                      color: 'rgba(255, 255, 255, 0.2)'
-                  }
-              }]
-          }
-      }
-  });
-}
-
-// Tabell utsläpp2
-if (document.getElementById("utslapp-statistik2")) {
-  var ctx = document.getElementById("utslapp-statistik2").getContext('2d');
-  var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-          labels: ["1970", "1990", "2016"],
-          datasets: [{
-              label: 'kg kött/person',
-              data: [50.6, 53.6, 82.2],
               backgroundColor: [
                   'rgba(54, 162, 235, 0.2)',
                   'rgba(240, 170 ,20 , 0.2)',
