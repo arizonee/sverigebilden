7d0c758e35bbf0ae184a44ed8e38155cabcd4717
Ricardo Burrows <ricardoburrows@mrmacbook-sama.lan>
2018-01-08 17:08:26 +0100

    Fyllt på narkotika och utsläpp med text och grafik

--- content/utslapp.php
+++ content/utslapp.php
@@ -5,17 +5,26 @@
     </div>
   </div>
   <div class="row">
-    <div class="col-sm-5">
-      <canvas id="utslapp-statistik1" width="250" height="250"></canvas>
+    <div class="col-lg-12 col-12">
+      <canvas id="utslapp-statistik1" width="250" height="125"></canvas>
     </div>
     <div class="col-sm-2">
     </div>
-    <div class="col-sm-5">
-      <canvas id="utslapp-statistik2" width="250" height="250"></canvas>
+    <div class="col-lg-12 col-12">
     </div>
   </div>
   <div class="row row-bottom">
     <div class="col-lg-12 col-12">
+        <p class="artikeltext">
+Sen den tidiga industrialiseringen har utsläppen i världen stadigt ökat. Under det man brukar kalla “den andra industriella revolutionen” mellan cirka 1871-1914 exploderade utvecklingen inom svensk industri, och många välkända företag såg dagens ljus. Bland annat LM Ericsson med sina uppfinningar kring telefonen och Bofors tack vare Alfred Nobels sprängämnen. I tiden efter andra världskriget ökade utvecklingen och 1947 låg Sverige över det globala snittet över utsläpp av koldioxid. Det skulle vi göra ända fram till 1977. Då vänder kurvan neråt mot minskade utsläpp och det har den gjort ända sedan dess. Iallafall här i Sverige.
+<br>
+<br>
+Jonas Allerup på Naturvårdsverket berättar att Sverige hade haft en minskning av sina utsläpp under många år och innan 1990 var de framför allt växthusgasutsläppen från oljepannor för uppvärmning av bostadshus som minskade då vi började gå över till andra bränslen och fjärrvärme. Han menar även att oljekrisen på 1970-talet kan vara en förklarande faktor till den drastiska minskningen. Även politiska vindar är en faktor till den svenska minskningen. Idag fortsätter den globala kurvan skarpt uppåt, samtidigt som den svenska utsläppskurvan sakta minskar för varje år. 
+<br>
+<br>
+Men myntet har flera sidor, och Naturvårdsverket skriver något tänkvärt på sin hemsida. 
+“Under de senaste tjugo åren har de konsumtionsbaserade utsläppen i Sverige minskat med trettio procent. Under samma period har andelen utsläpp som sker i andra länder, men som orsakats av vår svenska konsumtion ökat med nästan femtio procent.”
+           </p>
       <?php include("includes/share.php"); ?>
     </div>
   </div>
